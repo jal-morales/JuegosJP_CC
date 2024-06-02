37b295d64c551e3ad77b3d1106f5ee97e60e1082
jal-morales <jal.morales89@gmail.com>
2024-06-02 13:31:43 -0400

    Se termina el fronEnd de las vista que tendra la pagina web.

---
+++ Clases/clases.js
@@ -0,0 +1,17 @@
+export class tarjeta{
+    
+    constructor(precio,id,titulo){   
+      this.precio=precio;
+      this.id=id;
+      this.titulo=titulo;  
+    }
+}
+
+export class registro{
+    constructor(nombreUsuario,correo,password1,password2){
+        this.nombreUsuario=nombreUsuario;
+        this,correo=correo;
+        this.password1=password1;
+        this.password2=password2;
+    }
+}
---
+++ admin.js
@@ -0,0 +1,3 @@
+import data from "./api.json" with {type: "json"};
+import {tarjeta} from './Clases/clases.js'
+console.log(data);
